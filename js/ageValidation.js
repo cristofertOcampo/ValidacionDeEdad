ad4e8da975a2ee90d58c469e5240dd0b23325c60
cristofertOcampo <ocampocristofert@gmail.com>
2022-10-06 22:16:40 -0500

    Agregue un formulario que pueda ingresar su nombre y la edad que calcula si el usuario puede votar o no con javascript

---
+++ js/ageValidation.js
@@ -0,0 +1,10 @@
+function send(){
+  let userName = document.getElementById("UserName").value;
+  let userAge = parseFloat(document.getElementById("UserAge").value);
+  
+  if(userAge >= 18){
+    document.getElementById("paragraph").innerHTML =`Su nombre es ${userName} y tienes ${userAge} años, por lo tanto usted si puede votar` ;
+  }else{
+    document.getElementById("paragraph").innerHTML = `Su nombre es ${userName} y tienes ${userAge} años, por lo tanto usted no puede votar`;
+  }
+}
